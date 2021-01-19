adc690b3cc18e7f1120974ee3030ae099ecc5655
Stefan Froemken <froemken@gmail.com>
2021-01-19 11:00:13 +0100

    Streamline jobcenter with our other PF extensions

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/jobcenter.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+return [
+    \JWeiland\Jobcenter\Domain\Model\EmployerContact::class => [
+        'tableName' => 'tx_jobcenter_domain_model_employer_contact'
+    ]
+];
