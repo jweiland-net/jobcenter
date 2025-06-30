073388ac1b018d3cde6f590058111ffc489d1832
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-06-30 12:55:09 +0200

    [TASK] Updated configurations for the version

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Jobcenter',
     'description' => 'Recommend employer contacts based on conditions',
     'category' => 'plugin',
-    'author' => 'Stefan Froemken',
+    'author' => 'Stefan Froemken, Hoja Mustaffa Abdul Latheef',
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'version' => '3.2.1',
+    'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.36-11.5.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
