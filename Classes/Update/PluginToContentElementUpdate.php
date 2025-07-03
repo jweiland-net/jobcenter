3fdd3dbba3d436aaa0afd374db414c29e7b6850f
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-07-03 12:27:56 +0200

    [TASK] Fixed TYPO3 13 related fixes

---
+++ Classes/Update/PluginToContentElementUpdate.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/jobcenter.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\Jobcenter\Update;
+
+use TYPO3\CMS\Install\Attribute\UpgradeWizard;
+use TYPO3\CMS\Install\Updates\AbstractListTypeToCTypeUpdate;
+
+/**
+ * With TYPO3 13 all plugins have to be declared as content elements (CType) insteadof "list_type"
+ */
+#[UpgradeWizard('jobcenter_migratePluginsToContentElementsUpdate')]
+class PluginToContentElementUpdate extends AbstractListTypeToCTypeUpdate
+{
+    protected function getListTypeToCTypeMapping(): array
+    {
+        return [
+            'jobcenter_contact' => 'jobcenter_contact',
+            'jobcenter_employercontact' => 'jobcenter_employercontact',
+            'jobcenter_service' => 'jobcenter_service',
+        ];
+    }
+
+    public function getTitle(): string
+    {
+        return 'EXT:jobcenter - Migrate plugins to Content Elements';
+    }
+
+    public function getDescription(): string
+    {
+        return 'The modern way to register plugins for TYPO3 is to register them as content element types. ' .
+            'Running this wizard will migrate all jobcenter plugins to content element (CType)';
+    }
+}
