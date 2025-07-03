1897771c3f470d28044683a562cbbb74af7a9bb1
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-07-03 12:11:45 +0200

    [TASK] Fixed header maps2 to jobcenter

--- Classes/Domain/Model/Contact.php
+++ Classes/Domain/Model/Contact.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the package jweiland/maps2.
+ * This file is part of the package jweiland/jobcenter.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
