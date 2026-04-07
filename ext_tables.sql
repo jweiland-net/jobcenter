5fc30d7bb6d8c3d8a15c26df9aed64512083fc61
Stefan Froemken <sfroemken@jweiland.net>
2026-04-07 17:01:58 +0200

    [TASK] Remove unused database tables and fields
    - Dropped tx_jobcenter_domain_model_contact table
    - Dropped tx_jobcenter_domain_model_employer_contact table
    - Removed zip field in tx_jobcenter_domain_model_zip table
    - Cleaned up database schema to enhance maintainability

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,44 +1,9 @@
 #
-# Table structure for table 'tx_jobcenter_domain_model_contact'
-#
-CREATE TABLE tx_jobcenter_domain_model_contact
-(
-	salutation    int(11) DEFAULT '0' NOT NULL,
-	name          varchar(255) DEFAULT '' NOT NULL,
-	address       varchar(255) DEFAULT '' NOT NULL,
-	room_number   varchar(255) DEFAULT '' NOT NULL,
-	telephone     varchar(255) DEFAULT '' NOT NULL,
-	handicapped   tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	self_reliance tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	letters       int(11) unsigned DEFAULT '0' NOT NULL,
-	is_fallback   tinyint(4) unsigned DEFAULT '0' NOT NULL,
-);
-
-#
-# Table structure for table 'tx_jobcenter_domain_model_employer_contact'
-#
-CREATE TABLE tx_jobcenter_domain_model_employer_contact
-(
-	salutation  int(11) DEFAULT '0' NOT NULL,
-	first_name  varchar(255) DEFAULT '' NOT NULL,
-	last_name   varchar(255) DEFAULT '' NOT NULL,
-	address     varchar(255) DEFAULT '' NOT NULL,
-	zip         int(11) DEFAULT '0' NOT NULL,
-	room_number varchar(255) DEFAULT '' NOT NULL,
-	telephone   varchar(255) DEFAULT '' NOT NULL,
-	email       varchar(255) DEFAULT '' NOT NULL,
-	image       int(11) DEFAULT '0' NOT NULL,
-	is_fallback tinyint(4) unsigned DEFAULT '0' NOT NULL,
-);
-
-#
 # Table structure for table 'tx_jobcenter_domain_model_letter'
 #
 CREATE TABLE tx_jobcenter_domain_model_letter
 (
-	contact      int(11) unsigned DEFAULT '0' NOT NULL,
-	letter_start varchar(255) DEFAULT '' NOT NULL,
-	letter_end   varchar(255) DEFAULT '' NOT NULL,
+	contact int(11) unsigned DEFAULT '0' NOT NULL,
 );
 
 #
@@ -47,5 +12,4 @@ CREATE TABLE tx_jobcenter_domain_model_letter
 CREATE TABLE tx_jobcenter_domain_model_zip
 (
 	employer_contact int(11) unsigned DEFAULT '0' NOT NULL,
-	zip              varchar(255) DEFAULT '' NOT NULL,
 );
